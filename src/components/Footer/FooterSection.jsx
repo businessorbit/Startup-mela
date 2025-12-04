27ad8922333061abbb753d8d7181b99dd4420075
nitin-011 <chalananitin161@gmail.com>
2025-12-04 14:30:59 +0530

    final deploy

--- src/components/Footer/FooterSection.jsx
+++ src/components/Footer/FooterSection.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import AnimatedBg from "../AnimatedBg/AnimatedBg";
 
 const footerLinks = [
+
   { label: "Agenda", href: "#agenda" },
   { label: "Venue", href: "/" },
   { label: "Sponsors", href: "/sponsors" },
