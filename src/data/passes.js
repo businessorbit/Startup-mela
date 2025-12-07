4fa750d7179d333c398917f250cc8767a9e893e3
nitin-011 <chalananitin161@gmail.com>
2025-12-07 14:02:04 +0530

    error

---
+++ src/data/passes.js
@@ -0,0 +1,52 @@
+export const passes = [
+  {
+    id: 1,
+    title: "General Entry Pass",
+    price: "₹60",
+    oldPrice: null,
+    features: [
+      "Entry to Exhibitor Zone (Zone 1)",
+      "Explore 100+ startup stalls",
+      "Product demos & brand showcases",
+      "Food Court access",
+      "Vibrant festival experience"
+    ],
+    popular: false
+  },
+  {
+    id: 2,
+    title: "Arena Pass",
+    price: "₹1,420",
+    oldPrice: null, 
+    features: [
+      "Everything in the General Entry Pass",
+      "Entry to Stage Side (Zone 2)",
+      "Public viewing of Main Stage events"
+    ],
+    popular: true
+  },
+  {
+    id: 3,
+    title: "All-Access Conference Pass",
+    price: "₹3,520",
+    oldPrice: null,
+    features: [
+      "Everything in the Arena Pass",
+      "Exclusive entry to Conference Zone (Zone 3)",
+      "Access to the Knowledge Hub (All expert sessions)"
+    ],
+    popular: false
+  },
+  {
+    id: 4,
+    title: "Startup Pitching Pass",
+    price: "₹20,000",
+    oldPrice: null,
+    features: [
+      "Reserved entry to the Official Pitching Event",
+      "Present your startup to VCs & angel investors",
+      "High-stakes fundraising opportunity"
+    ],
+    popular: false
+  }
+];
